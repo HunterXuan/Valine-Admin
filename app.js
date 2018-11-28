79b29fefc3de0315ec1a48c9ce1593a092c3be50
HunterX <endpot@gmail.com>
2018-11-28 22:58:06 +0800

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(AV.Cloud.CookieSession({ secret: 'my secret', maxAge: 3600000, fetchUser
 // add headers
 app.use(function(req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://hunterx.xyz');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
